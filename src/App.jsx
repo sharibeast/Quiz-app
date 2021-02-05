10b9932e6071a29c7e3bd850e5063f1246691144
sharibeast <sharibeast@gmail.com>
2021-02-05 14:09:02 +0300

    new game button

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,14 @@
 import React, { useState } from "react";
+import questions from './data'
 
 const App = () => {
-  const questions = [
-    {
-      questionText: "What is the capital of france?",
-      answerOptions: [
-        { answerText: "New York", isCorrect: false },
-        { answerText: "London", isCorrect: false },
-        { answerText: "Paris", isCorrect: true },
-        { answerText: "Dublin", isCorrect: false },
-      ],
-    },
-    {
-      questionText: "What is the CEO of Tesla?",
-      answerOptions: [
-        { answerText: "Jeff Bezos", isCorrect: false },
-        { answerText: "Elon Musk", isCorrect: true },
-        { answerText: "Bill Gates", isCorrect: false },
-        { answerText: "Tony Stark", isCorrect: false },
-      ],
-    },
-  ];
-
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const[showScore,setShowScore]=useState(false);
-  const [score,setScore] =useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
 
   const handleAnswerButtonClick = (isCorrect) => {
-    if(isCorrect===true){
-      setScore(score+1);
+    if (isCorrect === true) {
+      setScore(score + 1);
     }
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
@@ -37,15 +17,25 @@ const App = () => {
       setShowScore(true);
     }
   };
+const handleNewGameClick = (params) => {
+  setCurrentQuestion(0)
+  setShowScore(false)
+  setScore(0)
+}
   return (
     <div className="app">
       {showScore ? (
-        <div>You scored 1 out of {questions.lengt}</div>
+        <div>
+          <div>
+          You scored {score} out of {questions.length}
+        </div>
+        <button className='new-game' onClick={handleNewGameClick}>New game</button>
+        </div>
       ) : (
         <div>
           <section className="question-section">
             <div className="question-count">
-              <span>Question {currentQuestion+1}</span>/{questions.length}
+              <span>Question {currentQuestion + 1}</span>/{questions.length}
             </div>
             <div className="question-text">
               {questions[currentQuestion].questionText}
@@ -53,7 +43,7 @@ const App = () => {
           </section>
           <section className="answer-section">
             {questions[currentQuestion].answerOptions.map((item) => (
-              <button onClick={()=>handleAnswerButtonClick(item.isCorrect)}>
+              <button onClick={() => handleAnswerButtonClick(item.isCorrect)}>
                 {item.answerText}
               </button>
             ))}
---
+++ src/data.js
@@ -0,0 +1,76 @@
+const questions = [
+    {
+      questionText: "What is the capital of france?",
+      answerOptions: [
+        { answerText: "New York", isCorrect: false },
+        { answerText: "London", isCorrect: false },
+        { answerText: "Paris", isCorrect: true },
+        { answerText: "Dublin", isCorrect: false },
+      ],
+    },
+    {
+      questionText: "What is the CEO of Tesla?",
+      answerOptions: [
+        { answerText: "Jeff Bezos", isCorrect: false },
+        { answerText: "Elon Musk", isCorrect: true },
+        { answerText: "Bill Gates", isCorrect: false },
+        { answerText: "Tony Stark", isCorrect: false },
+      ],
+    },
+    {
+        questionText: "What is the capital of france?",
+        answerOptions: [
+          { answerText: "New York", isCorrect: false },
+          { answerText: "London", isCorrect: false },
+          { answerText: "Paris", isCorrect: true },
+          { answerText: "Dublin", isCorrect: false },
+        ],
+      },
+      {
+        questionText: "What is the CEO of Tesla?",
+        answerOptions: [
+          { answerText: "Jeff Bezos", isCorrect: false },
+          { answerText: "Elon Musk", isCorrect: true },
+          { answerText: "Bill Gates", isCorrect: false },
+          { answerText: "Tony Stark", isCorrect: false },
+        ],
+      },
+      {
+        questionText: "What is the capital of france?",
+        answerOptions: [
+          { answerText: "New York", isCorrect: false },
+          { answerText: "London", isCorrect: false },
+          { answerText: "Paris", isCorrect: true },
+          { answerText: "Dublin", isCorrect: false },
+        ],
+      },
+      {
+        questionText: "What is the CEO of Tesla?",
+        answerOptions: [
+          { answerText: "Jeff Bezos", isCorrect: false },
+          { answerText: "Elon Musk", isCorrect: true },
+          { answerText: "Bill Gates", isCorrect: false },
+          { answerText: "Tony Stark", isCorrect: false },
+        ],
+      },
+      {
+        questionText: "What is the capital of france?",
+        answerOptions: [
+          { answerText: "New York", isCorrect: false },
+          { answerText: "London", isCorrect: false },
+          { answerText: "Paris", isCorrect: true },
+          { answerText: "Dublin", isCorrect: false },
+        ],
+      },
+      {
+        questionText: "What is the CEO of Tesla?",
+        answerOptions: [
+          { answerText: "Jeff Bezos", isCorrect: false },
+          { answerText: "Elon Musk", isCorrect: true },
+          { answerText: "Bill Gates", isCorrect: false },
+          { answerText: "Tony Stark", isCorrect: false },
+        ],
+      }
+  ];
+
+  export default questions
